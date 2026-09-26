08c58fd38b50d54afda26e227f9bad30fd1c90b3
secaudit <secaudit@localhost>
2026-09-26 04:43:38 -0700

    secaudit secaudit/instance_flipt-io__flipt-e5fe37c379e1eec2dd3492c5737c0be761050b26/Claude_4.5_Sonnet__SWE-agent/before

---
+++ build/testing/cli.go
@@ -0,0 +1,375 @@
+package testing
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"dagger.io/dagger"
+)
+
+func CLI(ctx context.Context, client *dagger.Client, container *dagger.Container) error {
+	{
+		container := container.Pipeline("flipt --help")
+		expected, err := os.ReadFile("build/testing/testdata/cli.txt")
+		if err != nil {
+			return err
+		}
+
+		if _, err := assertExec(ctx, container, flipt("--help"),
+			fails,
+			stdout(equals(expected))); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt --version")
+		if _, err := assertExec(ctx, container, flipt("--version"),
+			fails,
+			stdout(contains("Commit:")),
+			stdout(matches(`Build Date: [0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z`)),
+			stdout(matches(`Go Version: go[0-9]+\.[0-9]+\.[0-9]`)),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt --config")
+		if _, err := assertExec(ctx, container, flipt("foo"),
+			fails,
+			stderr(contains(`unknown command "foo" for "flipt"
+Run 'flipt --help' for usage.`))); err != nil {
+			return err
+		}
+
+		if _, err := assertExec(ctx, container, flipt("--config", "/foo/bar.yml"),
+			fails,
+			stderr(contains("loading configuration: open /foo/bar.yml: no such file or directory")),
+		); err != nil {
+			return err
+		}
+
+		if _, err := assertExec(ctx, container, flipt("--config", "/tmp"),
+			fails,
+			stderr(contains(`loading configuration: Unsupported Config Type`)),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt (no config)")
+		if _, err := assertExec(ctx, container.WithExec([]string{"rm", "/etc/flipt/config/default.yml"}), flipt(),
+			fails,
+			stderr(contains(`loading configuration: open /etc/flipt/config/default.yml: no such file or directory`)),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt (user config directory)")
+		container = container.
+			WithExec([]string{"mkdir", "-p", "/home/flipt/.config/flipt"}).
+			WithFile("/home/flipt/.config/flipt/config.yml", client.Host().Directory("build/testing/testdata").File("default.yml")).
+			// in order to stop a blocking process via SIGTERM and capture a successful exit code
+			// we use a shell script to start flipt in the background, sleep for two seconds,
+			// send the SIGTERM signal, wait for process to exit and then propagate Flipts exit code
+			WithNewFile("/test.sh", dagger.ContainerWithNewFileOpts{
+				Contents: `#!/bin/sh
+
+/flipt &
+
+sleep 2
+
+kill -s TERM $!
+
+wait $!
+
+exit $?`,
+				Owner:       "flipt",
+				Permissions: 0777,
+			}).
+			WithEnvVariable("FLIPT_LOG_LEVEL", "debug")
+
+		if _, err := assertExec(ctx, container, []string{"/test.sh"},
+			stdout(contains("configuration source\t{\"path\": \"/home/flipt/.config/flipt/config.yml\"}")),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt import/export")
+
+		var err error
+		if _, err = assertExec(ctx, container,
+			sh("echo FOOBAR | /flipt import --stdin"),
+			fails,
+		); err != nil {
+			return err
+		}
+
+		if _, err = assertExec(ctx, container,
+			flipt("import", "foo"),
+			fails,
+			stderr(contains("opening import file: open foo: no such file or directory")),
+		); err != nil {
+			return err
+		}
+		opts := dagger.ContainerWithFileOpts{
+			Owner: "flipt",
+		}
+
+		container = container.WithFile("/tmp/flipt.yml",
+			client.Host().Directory("build/testing/testdata").File("flipt.yml"), opts)
+
+		// import via STDIN succeeds
+		if _, err = assertExec(ctx, container, sh("cat /tmp/flipt.yml | /flipt import --stdin")); err != nil {
+			return err
+		}
+
+		// import valid yaml path and retrieve resulting container
+		container, err = assertExec(ctx, container, flipt("import", "/tmp/flipt.yml"))
+		if err != nil {
+			return err
+		}
+
+		if _, err = assertExec(ctx, container,
+			flipt("export"),
+			stdout(contains(expectedFliptYAML)),
+		); err != nil {
+			return err
+		}
+
+		container, err = assertExec(ctx, container,
+			flipt("export", "-o", "/tmp/export.yml"),
+		)
+		if err != nil {
+			return err
+		}
+
+		contents, err := container.File("/tmp/export.yml").Contents(ctx)
+		if err != nil {
+			return err
+		}
+
+		if !strings.Contains(contents, expectedFliptYAML) {
+			return fmt.Errorf("unexpected output: %q does not contain %q", contents, expectedFliptYAML)
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt export with mutually exclusive flags")
+
+		_, err := assertExec(ctx, container,
+			flipt("export", "--all-namespaces", "--namespaces", "foo,bar"),
+			fails,
+			stderr(contains("if any flags in the group [all-namespaces namespaces namespace]")),
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt import")
+
+		opts := dagger.ContainerWithFileOpts{
+			Owner: "flipt",
+		}
+
+		container = container.WithFile("/tmp/flipt.yml",
+			client.Host().Directory("build/testing/testdata").File("flipt-namespace-foo.yml"), opts)
+
+		// import via STDIN succeeds
+		_, err := assertExec(ctx, container, sh("cat /tmp/flipt.yml | /flipt import --stdin"))
+		if err != nil {
+			return err
+		}
+
+		// import valid yaml path and retrieve resulting container
+		container, err = assertExec(ctx, container, flipt("import", "/tmp/flipt.yml"))
+		if err != nil {
+			return err
+		}
+
+		if _, err = assertExec(ctx, container,
+			flipt("export", "--namespaces", "foo"),
+			stdout(contains(expectedFliptYAML)),
+		); err != nil {
+			return err
+		}
+
+		container, err = assertExec(ctx, container,
+			flipt("export", "--namespace", "foo", "-o", "/tmp/export.yml"),
+		)
+		if err != nil {
+			return err
+		}
+
+		contents, err := container.File("/tmp/export.yml").Contents(ctx)
+		if err != nil {
+			return err
+		}
+
+		if !strings.Contains(contents, expectedFliptYAML) {
+			return fmt.Errorf("unexpected output: %q does not contain %q", contents, expectedFliptYAML)
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt import YAML stream")
+
+		opts := dagger.ContainerWithFileOpts{
+			Owner: "flipt",
+		}
+
+		container = container.WithFile("/tmp/flipt.yml",
+			client.Host().Directory("build/testing/testdata").File("flipt-yaml-stream.yml"),
+			opts,
+		)
+
+		container, err := assertExec(ctx, container, sh("cat /tmp/flipt.yml | /flipt import --stdin"))
+		if err != nil {
+			return err
+		}
+
+		if _, err := assertExec(ctx, container,
+			flipt("export", "--all-namespaces"),
+			stdout(contains(expectedYAMLStreamOutput)),
+		); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt migrate")
+		if _, err := assertExec(ctx, container, flipt("migrate")); err != nil {
+			return err
+		}
+	}
+
+	{
+		container := container.Pipeline("flipt config init")
+
+		container, err := assertExec(ctx, container, flipt("config", "init", "-y"))
+		if err != nil {
+			return err
+		}
+
+		contents, err := container.File("/home/flipt/.config/flipt/config.yml").Contents(ctx)
+		if err != nil {
+			return err
+		}
+
+		expected := `# yaml-language-server: $schema=https://raw.githubusercontent.com/flipt-io/flipt/main/config/flipt.schema.json`
+
+		if !strings.Contains(contents, expected) {
+			return fmt.Errorf("unexpected output: %q does not contain %q", contents, expected)
+		}
+	}
+
+	return nil
+}
+
+const (
+	expectedFliptYAML = `flags:
+- key: zUFtS7D0UyMeueYu
+  name: UAoZRksg94r1iipa
+  type: VARIANT_FLAG_TYPE
+  description: description
+  enabled: true
+  variants:
+  - key: NGxfcVffpMhBz9n8
+    name: fhDHQ7rcxvoaWbHw
+  - key: sDGD6NvfCRyaQUn3
+  rules:
+  - segment: 08UoVJ96LhZblPEx
+    distributions:
+    - variant: NGxfcVffpMhBz9n8
+      rollout: 100
+segments:
+- key: 08UoVJ96LhZblPEx
+  name: 2oS8SHbrxyFkRg1a
+  description: description
+  constraints:
+  - type: STRING_COMPARISON_TYPE
+    property: foo
+    operator: eq
+    value: baz
+  - type: STRING_COMPARISON_TYPE
+    property: fizz
+    operator: neq
+    value: buzz
+`
+
+	expectedYAMLStreamOutput = `version: "1.2"
+namespace: default
+---
+namespace: foo
+flags:
+- key: zUFtS7D0UyMeueYu
+  name: UAoZRksg94r1iipa
+  type: VARIANT_FLAG_TYPE
+  description: description
+  enabled: true
+  variants:
+  - key: NGxfcVffpMhBz9n8
+    name: fhDHQ7rcxvoaWbHw
+  - key: sDGD6NvfCRyaQUn3
+  rules:
+  - segment: 08UoVJ96LhZblPEx
+    distributions:
+    - variant: NGxfcVffpMhBz9n8
+      rollout: 100
+segments:
+- key: 08UoVJ96LhZblPEx
+  name: 2oS8SHbrxyFkRg1a
+  description: description
+  constraints:
+  - type: STRING_COMPARISON_TYPE
+    property: foo
+    operator: eq
+    value: baz
+  - type: STRING_COMPARISON_TYPE
+    property: fizz
+    operator: neq
+    value: buzz
+  match_type: ALL_MATCH_TYPE
+---
+namespace: bar
+flags:
+- key: zUFtS7D0UyMeueYu
+  name: UAoZRksg94r1iipa
+  type: VARIANT_FLAG_TYPE
+  description: description
+  enabled: true
+  variants:
+  - key: NGxfcVffpMhBz9n8
+    name: fhDHQ7rcxvoaWbHw
+  - key: sDGD6NvfCRyaQUn3
+  rules:
+  - segment: 08UoVJ96LhZblPEx
+    distributions:
+    - variant: NGxfcVffpMhBz9n8
+      rollout: 100
+segments:
+- key: 08UoVJ96LhZblPEx
+  name: 2oS8SHbrxyFkRg1a
+  description: description
+  constraints:
+  - type: STRING_COMPARISON_TYPE
+    property: foo
+    operator: eq
+    value: baz
+  - type: STRING_COMPARISON_TYPE
+    property: fizz
+    operator: neq
+    value: buzz
+  match_type: ALL_MATCH_TYPE
+`
+)
---
+++ cmd/flipt/import.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"go.flipt.io/flipt/internal/ext"
+	"go.flipt.io/flipt/internal/storage/sql"
+	"go.uber.org/zap"
+)
+
+type importCommand struct {
+	dropBeforeImport bool
+	importStdin      bool
+	address          string
+	token            string
+}
+
+func newImportCommand() *cobra.Command {
+	importCmd := &importCommand{}
+
+	cmd := &cobra.Command{
+		Use:   "import",
+		Short: "Import Flipt data from file/stdin",
+		RunE:  importCmd.run,
+	}
+
+	cmd.Flags().BoolVar(
+		&importCmd.dropBeforeImport,
+		"drop",
+		false,
+		"drop database before import",
+	)
+
+	cmd.Flags().BoolVar(
+		&importCmd.importStdin,
+		"stdin",
+		false,
+		"import from STDIN",
+	)
+
+	cmd.Flags().StringVarP(
+		&importCmd.address,
+		"address", "a",
+		"",
+		"address of remote Flipt instance to import into (defaults to direct DB import if not supplied)",
+	)
+
+	cmd.Flags().StringVarP(
+		&importCmd.token,
+		"token", "t",
+		"",
+		"client token used to authenticate access to remote Flipt instance when importing.",
+	)
+
+	cmd.Flags().StringVar(&providedConfigFile, "config", "", "path to config file")
+	return cmd
+}
+
+func (c *importCommand) run(cmd *cobra.Command, args []string) error {
+	var (
+		in     io.Reader = os.Stdin
+		logger           = zap.Must(zap.NewDevelopment())
+	)
+
+	if !c.importStdin {
+		if len(args) < 1 {
+			return errors.New("import filename required")
+		}
+
+		importFilename := args[0]
+		if importFilename == "" {
+			return errors.New("import filename required")
+		}
+
+		f := filepath.Clean(importFilename)
+
+		logger.Debug("importing", zap.String("source_path", f))
+
+		fi, err := os.Open(f)
+		if err != nil {
+			return fmt.Errorf("opening import file: %w", err)
+		}
+
+		defer fi.Close()
+
+		in = fi
+	}
+
+	// Use client when remote address is configured.
+	if c.address != "" {
+		client, err := fliptClient(c.address, c.token)
+		if err != nil {
+			return err
+		}
+		return ext.NewImporter(client).Import(cmd.Context(), in)
+	}
+
+	logger, cfg, err := buildConfig()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	// drop tables if specified
+	if c.dropBeforeImport {
+		logger.Debug("dropping tables")
+
+		migrator, err := sql.NewMigrator(*cfg, logger)
+		if err != nil {
+			return err
+		}
+
+		if err := migrator.Drop(); err != nil {
+			return fmt.Errorf("attempting to drop: %w", err)
+		}
+
+		if _, err := migrator.Close(); err != nil {
+			return fmt.Errorf("closing migrator: %w", err)
+		}
+	}
+
+	migrator, err := sql.NewMigrator(*cfg, logger)
+	if err != nil {
+		return err
+	}
+
+	if err := migrator.Up(forceMigrate); err != nil {
+		return err
+	}
+
+	if _, err := migrator.Close(); err != nil {
+		return fmt.Errorf("closing migrator: %w", err)
+	}
+
+	// Otherwise, go direct to the DB using Flipt configuration file.
+	server, cleanup, err := fliptServer(logger, cfg)
+	if err != nil {
+		return err
+	}
+
+	defer cleanup()
+
+	return ext.NewImporter(
+		server,
+	).Import(cmd.Context(), in)
+}
