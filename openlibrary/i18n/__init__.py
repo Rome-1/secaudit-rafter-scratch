73571f020b2043ce76588b06ac88b05a9e514904
secaudit <secaudit@localhost>
2026-09-26 03:09:02 -0700

    secaudit secaudit/instance_internetarchive__openlibrary-7cbfb812ef0e1f9716e2d6e85d538a96fcb79d13-vfa6ff903cb27f336e17654595dd900fa943dcd91/Claude_4_Sonnet__SWE-agent/before

---
+++ openlibrary/i18n/__init__.py
@@ -0,0 +1,179 @@
+from __future__ import print_function
+import web
+import os
+
+import babel
+from babel._compat import BytesIO
+from babel.support import Translations
+from babel.messages import Catalog
+from babel.messages.pofile import read_po, write_po
+from babel.messages.mofile import write_mo
+from babel.messages.extract import extract_from_file, extract_from_dir, extract_python
+
+root = os.path.dirname(__file__)
+
+def _compile_translation(po, mo):
+    try:
+        catalog = read_po(open(po, 'rb'))
+
+        f = open(mo, 'wb')
+        write_mo(f, catalog)
+        f.close()
+        print('compiled', po, file=web.debug)
+    except Exception as e:
+        print('failed to compile', po, file=web.debug)
+        raise e
+
+def get_locales():
+    return [d for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))]
+
+def extract_templetor(fileobj, keywords, comment_tags, options):
+    """Extract i18n messages from web.py templates."""
+    try:
+        instring = fileobj.read().decode('utf-8')
+        # Replace/remove inline js '\$' which interferes with the Babel python parser:
+        cleaned_string = instring.replace('\$', '')
+        code = web.template.Template.generate_code(cleaned_string, fileobj.name)
+        f = BytesIO(code.encode('utf-8')) # Babel wants bytes, not strings
+    except Exception as e:
+        print('Failed to extract ' + fileobj.name + ':', repr(e), file=web.debug)
+        return []
+    return extract_python(f, keywords, comment_tags, options)
+
+def extract_messages(dirs):
+    catalog = Catalog(
+        project='Open Library',
+        copyright_holder='Internet Archive'
+    )
+    METHODS = [
+        ("**.py", "python"),
+        ("**.html", "openlibrary.i18n:extract_templetor")
+    ]
+    COMMENT_TAGS = ["NOTE:"]
+
+    for d in dirs:
+        if '.html' in d:
+            extracted = [(d,) + extract for extract in extract_from_file("openlibrary.i18n:extract_templetor", d)]
+        else:
+            extracted = extract_from_dir(d, METHODS, comment_tags=COMMENT_TAGS, strip_comment_tags=True)
+        for filename, lineno, message, comments, context in extracted:
+            catalog.add(message, None, [(filename, lineno)], auto_comments=comments)
+
+    path = os.path.join(root, 'messages.pot')
+    f = open(path, 'wb')
+    write_po(f, catalog)
+    f.close()
+
+    print('wrote template to', path)
+
+def compile_translations():
+    for locale in get_locales():
+        po_path = os.path.join(root, locale, 'messages.po')
+        mo_path = os.path.join(root, locale, 'messages.mo')
+
+        if os.path.exists(po_path):
+            _compile_translation(po_path, mo_path)
+
+def update_translations():
+    pot_path = os.path.join(root, 'messages.pot')
+    template = read_po(open(pot_path, 'rb'))
+
+    for locale in get_locales():
+        po_path = os.path.join(root, locale, 'messages.po')
+        mo_path = os.path.join(root, locale, 'messages.mo')
+
+        if os.path.exists(po_path):
+            catalog = read_po(open(po_path, 'rb'))
+            catalog.update(template)
+
+            f = open(po_path, 'wb')
+            write_po(f, catalog)
+            f.close()
+            print('updated', po_path)
+
+    compile_translations()
+
+@web.memoize
+def load_translations(lang):
+    po = os.path.join(root, lang, 'messages.po')
+    mo_path = os.path.join(root, lang, 'messages.mo')
+
+    if os.path.exists(mo_path):
+        return Translations(open(mo_path, 'rb'))
+
+@web.memoize
+def load_locale(lang):
+    try:
+        return babel.Locale(lang)
+    except babel.UnknownLocaleError:
+        pass
+
+class GetText:
+    def __call__(self, string, *args, **kwargs):
+        """Translate a given string to the language of the current locale."""
+        # Get the website locale from the global ctx.lang variable, set in i18n_loadhook
+        translations = load_translations(web.ctx.lang)
+        value = (translations and translations.ugettext(string)) or string
+
+        if args:
+            value = value % args
+        elif kwargs:
+            value = value % kwargs
+
+        return value
+
+    def __getattr__(self, key):
+        from infogami.utils.i18n import strings
+        # for backward-compatability
+        return strings.get('', key)
+
+class LazyGetText:
+    def __call__(self, string, *args, **kwargs):
+        """Translate a given string lazily."""
+        return LazyObject(lambda: GetText()(string, *args, **kwargs))
+
+class LazyObject:
+    def __init__(self, creator):
+        self._creator = creator
+
+    def __str__(self):
+        return web.safestr(self._creator())
+
+    def __repr__(self):
+        return repr(self._creator())
+
+    def __add__(self, other):
+        return self._creator() + other
+
+    def __radd__(self, other):
+        return other + self._creator()
+
+
+def ungettext(s1, s2, _n, *a, **kw):
+    # Get the website locale from the global ctx.lang variable, set in i18n_loadhook
+    translations = load_translations(web.ctx.lang)
+    value = translations and translations.ungettext(s1, s2, _n)
+    if not value:
+        # fallback when translation is not provided
+        if _n == 1:
+            value = s1
+        else:
+            value = s2
+
+    if a:
+        return value % a
+    elif kw:
+        return value % kw
+    else:
+        return value
+
+def gettext_territory(code):
+    """Returns the territory name in the current locale."""
+    # Get the website locale from the global ctx.lang variable, set in i18n_loadhook
+    locale = load_locale(web.ctx.lang)
+    return locale.territories.get(code, code)
+
+gettext = GetText()
+ugettext = gettext
+lgettext = LazyGetText()
+_ = gettext
---
+++ openlibrary/plugins/upstream/code.py
@@ -0,0 +1,329 @@
+"""Upstream customizations."""
+
+import datetime
+import hashlib
+import io
+import os.path
+import random
+
+from six import PY3
+
+import web
+
+from infogami import config
+from infogami.core import code as core
+from infogami.infobase import client
+from infogami.utils import delegate, app, types
+from infogami.utils.view import public, safeint, render
+from infogami.utils.view import render_template  # noqa: F401 used for its side effects
+from infogami.utils.context import context
+
+from openlibrary import accounts
+
+from openlibrary.plugins.upstream import addbook, covers, merge_authors, models, utils
+from openlibrary.plugins.upstream import spamcheck
+from openlibrary.plugins.upstream import borrow, recentchanges  # TODO: unused imports?
+from openlibrary.plugins.upstream.utils import render_component
+
+if not config.get('coverstore_url'):
+    config.coverstore_url = "https://covers.openlibrary.org"
+
+class static(delegate.page):
+    path = "/images/.*"
+    def GET(self):
+        host = 'https://%s' % web.ctx.host if 'openlibrary.org' in web.ctx.host else ''
+        raise web.seeother(host + '/static' + web.ctx.path)
+
+
+class edit(core.edit):
+    """Overwrite ?m=edit behaviour for author, book, work, and people pages."""
+    def GET(self, key):
+        page = web.ctx.site.get(key)
+
+        if web.re_compile('/(authors|books|works)/OL.*').match(key):
+            if page is None:
+                raise web.seeother(key)
+            else:
+                raise web.seeother(page.url(suffix="/edit"))
+        else:
+            return core.edit.GET(self, key)
+
+    def POST(self, key):
+        if web.re_compile('/(people/[^/]+)').match(key) and spamcheck.is_spam():
+            return render_template('message.html', 'Oops',
+                                   'Something went wrong. Please try again later.')
+        return core.edit.POST(self, key)
+
+
+
+# handlers for change photo and change cover
+
+class change_cover(delegate.mode):
+    path = "(/books/OL\d+M)/cover"
+
+    def GET(self, key):
+        page = web.ctx.site.get(key)
+        if page is None or page.type.key not in  ['/type/edition', '/type/author']:
+            raise web.seeother(key)
+        return render.change_cover(page)
+
+class change_photo(change_cover):
+    path = "(/authors/OL\d+A)/photo"
+
+del delegate.modes['change_cover']     # delete change_cover mode added by openlibrary plugin
+
+
+class components_test(delegate.page):
+    path = "/_dev/components/HelloWorld"
+
+    def GET(self):
+        return render_component('HelloWorld') + render_component('HelloWorld')
+
+
+class library_explorer(delegate.page):
+    path = "/explore"
+
+    def GET(self):
+        return render_template('library_explorer')
+
+
+class merge_work(delegate.page):
+    path = "(/works/OL\d+W)/merge"
+    def GET(self, key):
+        return "This looks like a good place for a merge UI!"
+
+    def POST(self, key):
+        pass
+
+@web.memoize
+@public
+def vendor_js():
+    pardir = os.path.pardir
+    path = os.path.abspath(os.path.join(__file__, pardir, pardir, pardir, pardir, 'static', 'upstream', 'js', 'vendor.js'))
+    with io.open(path, 'rb') as in_file:
+        digest = hashlib.md5(in_file.read()).hexdigest()
+    return '/static/upstream/js/vendor.js?v=' + digest
+
+@web.memoize
+@public
+def static_url(path):
+    """Takes path relative to static/ and constructs url to that resource with hash.
+    """
+    pardir = os.path.pardir
+    fullpath = os.path.abspath(os.path.join(__file__, pardir, pardir, pardir, pardir, "static", path))
+    with io.open(fullpath, 'rb') as in_file:
+        digest = hashlib.md5(in_file.read()).hexdigest()
+    return "/static/%s?v=%s" % (path, digest)
+
+class DynamicDocument:
+    """Dynamic document is created by concatinating various rawtext documents in the DB.
+    Used to generate combined js/css using multiple js/css files in the system.
+    """
+    def __init__(self, root):
+        self.root = web.rstrips(root, '/')
+        self.docs = None
+        self._text = None
+        self.last_modified = None
+
+    def update(self):
+        keys = web.ctx.site.things({'type': '/type/rawtext', 'key~': self.root + '/*'})
+        docs = sorted(web.ctx.site.get_many(keys), key=lambda doc: doc.key)
+        if docs:
+            self.last_modified = min(doc.last_modified for doc in docs)
+            self._text = "\n\n".join(doc.get('body', '') for doc in docs)
+        else:
+            self.last_modified = datetime.datetime.utcnow()
+            self._text = ""
+
+    def get_text(self):
+        """Returns text of the combined documents"""
+        if self._text is None:
+            self.update()
+        return self._text
+
+    def md5(self):
+        """Returns md5 checksum of the combined documents"""
+        return hashlib.md5(self.get_text().encode('utf-8')).hexdigest()
+
+def create_dynamic_document(url, prefix):
+    """Creates a handler for `url` for servering combined js/css for `prefix/*` pages"""
+    doc = DynamicDocument(prefix)
+
+    if url.endswith('.js'):
+        content_type = "text/javascript"
+    elif url.endswith(".css"):
+        content_type = "text/css"
+    else:
+        content_type = "text/plain"
+
+    class page(delegate.page):
+        """Handler for serving the combined content."""
+        path = "__registered_later_without_using_this__"
+        def GET(self):
+            i = web.input(v=None)
+            v = doc.md5()
+            if v != i.v:
+                raise web.seeother(web.changequery(v=v))
+
+            if web.modified(etag=v):
+                oneyear = 365 * 24 * 3600
+                web.header("Content-Type", content_type)
+                web.header("Cache-Control", "Public, max-age=%d" % oneyear)
+                web.lastmodified(doc.last_modified)
+                web.expires(oneyear)
+                return delegate.RawText(doc.get_text())
+
+        def url(self):
+            return url + "?v=" + doc.md5()
+
+        def reload(self):
+            doc.update()
+
+    class hook(client.hook):
+        """Hook to update the DynamicDocument when any of the source pages is updated."""
+        def on_new_version(self, page):
+            if page.key.startswith(doc.root):
+                doc.update()
+
+    # register the special page
+    delegate.pages[url] = {}
+    delegate.pages[url][None] = page
+    return page
+
+all_js = create_dynamic_document("/js/all.js", config.get("js_root", "/js"))
+web.template.Template.globals['all_js'] = all_js()
+
+all_css = create_dynamic_document("/css/all.css", config.get("css_root", "/css"))
+web.template.Template.globals['all_css'] = all_css()
+
+def reload():
+    """Reload all.css and all.js"""
+    all_css().reload()
+    all_js().reload()
+
+def setup_jquery_urls():
+    if config.get('use_google_cdn', True):
+        jquery_url = "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"
+        jqueryui_url = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"
+    else:
+        jquery_url = "/static/upstream/js/jquery-1.3.2.min.js"
+        jqueryui_url = "/static/upstream/js/jquery-ui-1.7.2.min.js"
+
+    web.template.Template.globals['jquery_url'] = jquery_url
+    web.template.Template.globals['jqueryui_url'] = jqueryui_url
+    web.template.Template.globals['use_google_cdn'] = config.get('use_google_cdn', True)
+
+
+def user_is_admin_or_librarian():
+    user = web.ctx.site.get_user()
+    return user and (user.is_admin() or user.is_librarian())
+
+
+@public
+def get_document(key, limit_redirs=5):
+    doc = None
+    for i in range(limit_redirs):
+        doc = web.ctx.site.get(key)
+        if doc is None:
+            return None
+        if doc.type.key == "/type/redirect":
+            key = doc.location
+        else:
+            return doc
+    return doc
+
+
+class revert(delegate.mode):
+    def GET(self, key):
+        raise web.seeother(web.changequery(m=None))
+
+    def POST(self, key):
+        i = web.input("v", _comment=None)
+        v = i.v and safeint(i.v, None)
+
+        if v is None:
+            raise web.seeother(web.changequery({}))
+
+        if not web.ctx.site.can_write(key) or not user_is_admin_or_librarian():
+            return render.permission_denied(web.ctx.fullpath, "Permission denied to edit " + key + ".")
+
+        thing = web.ctx.site.get(key, i.v)
+
+        if not thing:
+            raise web.notfound()
+
+        def revert(thing):
+            if thing.type.key == "/type/delete" and thing.revision > 1:
+                prev = web.ctx.site.get(thing.key, thing.revision-1)
+                if prev.type.key in ["/type/delete", "/type/redirect"]:
+                    return revert(prev)
+                else:
+                    prev._save("revert to revision %d" % prev.revision)
+                    return prev
+            elif thing.type.key == "/type/redirect":
+                redirect = web.ctx.site.get(thing.location)
+                if redirect and redirect.type.key not in ["/type/delete", "/type/redirect"]:
+                    return redirect
+                else:
+                    # bad redirect. Try the previous revision
+                    prev = web.ctx.site.get(thing.key, thing.revision-1)
+                    return revert(prev)
+            else:
+                return thing
+
+        def process(value):
+            if isinstance(value, list):
+                return [process(v) for v in value]
+            elif isinstance(value, client.Thing):
+                if value.key:
+                    if value.type.key in ['/type/delete', '/type/revert']:
+                        return revert(value)
+                    else:
+                        return value
+                else:
+                    for k in value:
+                        value[k] = process(value[k])
+                    return value
+            else:
+                return value
+
+        for k in thing:
+            thing[k] = process(thing[k])
+
+        comment = i._comment or "reverted to revision %d" % v
+        thing._save(comment)
+        raise web.seeother(key)
+
+def setup():
+    """Setup for upstream plugin"""
+    models.setup()
+    utils.setup()
+    addbook.setup()
+    covers.setup()
+    merge_authors.setup()
+
+    from openlibrary.plugins.upstream import data, jsdef
+    data.setup()
+
+    # setup template globals
+    from openlibrary.i18n import ugettext, ungettext, gettext_territory
+
+    web.template.Template.globals.update({
+        "gettext": ugettext,
+        "ugettext": ugettext,
+        "_": ugettext,
+        "ungettext": ungettext,
+        "gettext_territory": gettext_territory,
+        "random": random.Random(),
+        "commify": web.commify,
+        "group": web.group,
+        "storage": web.storage,
+        "all": all,
+        "any": any,
+        "locals": locals
+    })
+
+    web.template.STATEMENT_NODES["jsdef"] = jsdef.JSDefNode
+
+    setup_jquery_urls()
+setup()
