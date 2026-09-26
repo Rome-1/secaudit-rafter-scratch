04bbe0acb11d8fe3361cf585d3578a2dc87d6c9f
secaudit <secaudit@localhost>
2026-09-26 07:35:43 -0700

    secaudit secaudit/instance_flipt-io__flipt-c12967bc73fdf02054cf3ef8498c05e25f0a18c0/Claude_4.5_Sonnet__SWE-agent/before

---
+++ build/testing/integration.go
@@ -0,0 +1,304 @@
+package testing
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"dagger.io/dagger"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/uuid"
+	"golang.org/x/sync/errgroup"
+)
+
+var (
+	protocolPorts = map[string]string{"http": "8080", "grpc": "9000"}
+	replacer      = strings.NewReplacer(" ", "-", "/", "-")
+	sema          = make(chan struct{}, 6)
+
+	// AllCases are the top-level filterable integration test cases.
+	AllCases = map[string]testCaseFn{
+		"api":           api,
+		"fs/git":        git,
+		"fs/local":      local,
+		"import/export": importExport,
+	}
+)
+
+type testConfig struct {
+	name      string
+	namespace string
+	address   string
+	token     string
+}
+
+type testCaseFn func(_ context.Context, client *dagger.Client, base, flipt *dagger.Container, conf testConfig) func() error
+
+func filterCases(caseNames ...string) (map[string]testCaseFn, error) {
+	if len(caseNames) == 0 {
+		return AllCases, nil
+	}
+
+	cases := map[string]testCaseFn{}
+	for _, filter := range caseNames {
+		if _, ok := AllCases[filter]; !ok {
+			return nil, fmt.Errorf("unexpected test case filter: %q", filter)
+		}
+
+		cases[filter] = AllCases[filter]
+	}
+
+	return cases, nil
+}
+
+func Integration(ctx context.Context, client *dagger.Client, base, flipt *dagger.Container, caseNames ...string) error {
+	cases, err := filterCases(caseNames...)
+	if err != nil {
+		return err
+	}
+
+	logs := client.CacheVolume(fmt.Sprintf("logs-%s", uuid.New()))
+	_, err = flipt.WithUser("root").
+		WithMountedCache("/logs", logs).
+		WithExec([]string{"chown", "flipt:flipt", "/logs"}).
+		ExitCode(ctx)
+	if err != nil {
+		return err
+	}
+
+	var configs []testConfig
+
+	for _, namespace := range []string{"", "production"} {
+		for protocol, port := range protocolPorts {
+			for _, token := range []string{"", "some-token"} {
+				name := fmt.Sprintf("%s namespace %s", strings.ToUpper(protocol), namespace)
+				if token != "" {
+					name = fmt.Sprintf("%s with token %s", name, token)
+				}
+
+				configs = append(configs,
+					testConfig{
+						name:      name,
+						namespace: namespace,
+						address:   fmt.Sprintf("%s://flipt:%s", protocol, port),
+						token:     token,
+					},
+				)
+			}
+		}
+	}
+
+	rand.Seed(time.Now().Unix())
+
+	var g errgroup.Group
+
+	for caseName, fn := range cases {
+		for _, config := range configs {
+			var (
+				fn     = fn
+				config = config
+			)
+
+			flipt := flipt
+			if config.token != "" {
+				flipt = flipt.
+					WithEnvVariable("FLIPT_AUTHENTICATION_REQUIRED", "true").
+					WithEnvVariable("FLIPT_AUTHENTICATION_METHODS_TOKEN_ENABLED", "true").
+					WithEnvVariable("FLIPT_AUTHENTICATION_METHODS_TOKEN_BOOTSTRAP_TOKEN", config.token)
+			}
+
+			name := strings.ToLower(replacer.Replace(fmt.Sprintf("flipt-test-%s-config-%s", caseName, config.name)))
+			flipt = flipt.
+				WithEnvVariable("CI", os.Getenv("CI")).
+				WithEnvVariable("FLIPT_LOG_LEVEL", "debug").
+				WithEnvVariable("FLIPT_LOG_FILE", fmt.Sprintf("/var/opt/flipt/logs/%s.log", name)).
+				WithMountedCache("/var/opt/flipt/logs", logs)
+
+			g.Go(take(fn(ctx, client, base, flipt, config)))
+		}
+	}
+
+	err = g.Wait()
+
+	if _, lerr := client.Container().From("alpine:3.16").
+		WithMountedCache("/logs", logs).
+		WithExec([]string{"cp", "-r", "/logs", "/out"}).
+		Directory("/out").Export(ctx, "build/logs"); lerr != nil {
+		log.Println("Error copying logs", lerr)
+	}
+
+	return err
+}
+
+func take(fn func() error) func() error {
+	return func() error {
+		// insert into semaphore channel to maintain
+		// a max concurrency
+		sema <- struct{}{}
+		defer func() { <-sema }()
+
+		return fn()
+	}
+}
+
+func api(ctx context.Context, _ *dagger.Client, base, flipt *dagger.Container, conf testConfig) func() error {
+	return suite(ctx, "api", base,
+		// create unique instance for test case
+		flipt.
+			WithEnvVariable("UNIQUE", uuid.New().String()).
+			WithExec(nil), conf)
+}
+
+const (
+	testdataDir     = "build/testing/integration/readonly/testdata"
+	testdataPathFmt = testdataDir + "/%s.yaml"
+)
+
+func local(ctx context.Context, client *dagger.Client, base, flipt *dagger.Container, conf testConfig) func() error {
+	flipt = flipt.
+		WithDirectory("/tmp/testdata", base.Directory(testdataDir)).
+		WithEnvVariable("FLIPT_LOG_LEVEL", "DEBUG").
+		WithEnvVariable("FLIPT_EXPERIMENTAL_FILESYSTEM_STORAGE_ENABLED", "true").
+		WithEnvVariable("FLIPT_STORAGE_TYPE", "local").
+		WithEnvVariable("FLIPT_STORAGE_LOCAL_PATH", "/tmp/testdata").
+		WithEnvVariable("UNIQUE", uuid.New().String()).
+		WithExec(nil)
+
+	return suite(ctx, "readonly", base, flipt, conf)
+}
+
+func git(ctx context.Context, client *dagger.Client, base, flipt *dagger.Container, conf testConfig) func() error {
+	gitea := client.Container().
+		From("gitea/gitea:latest").
+		WithExposedPort(3000).
+		WithExec(nil)
+
+	_, err := base.
+		WithServiceBinding("gitea", gitea).
+		WithExec([]string{"go", "run", "./build/internal/cmd/gitea/...", "-gitea-url", "http://gitea:3000", "-testdata-dir", testdataDir}).
+		Sync(ctx)
+	if err != nil {
+		return func() error { return err }
+	}
+
+	flipt = flipt.
+		WithServiceBinding("gitea", gitea).
+		WithEnvVariable("FLIPT_LOG_LEVEL", "DEBUG").
+		WithEnvVariable("FLIPT_EXPERIMENTAL_FILESYSTEM_STORAGE_ENABLED", "true").
+		WithEnvVariable("FLIPT_STORAGE_TYPE", "git").
+		WithEnvVariable("FLIPT_STORAGE_GIT_REPOSITORY", "http://gitea:3000/root/features.git").
+		WithEnvVariable("UNIQUE", uuid.New().String()).
+		WithExec(nil)
+
+	return suite(ctx, "readonly", base, flipt, conf)
+}
+
+func importExport(ctx context.Context, _ *dagger.Client, base, flipt *dagger.Container, conf testConfig) func() error {
+	return func() error {
+		// import testdata before running readonly suite
+		flags := []string{"--address", conf.address}
+		if conf.token != "" {
+			flags = append(flags, "--token", conf.token)
+		}
+
+		ns := "default"
+		if conf.namespace != "" {
+			ns = conf.namespace
+			flags = append(flags, "--namespace", conf.namespace)
+		}
+
+		seed := base.File(fmt.Sprintf(testdataPathFmt, ns))
+
+		var (
+			// create unique instance for test case
+			fliptToTest = flipt.
+					WithEnvVariable("UNIQUE", uuid.New().String()).
+					WithExec(nil)
+
+			importCmd = append([]string{"/flipt", "import"}, append(flags, "--create-namespace", "import.yaml")...)
+		)
+		// use target flipt binary to invoke import
+		_, err := flipt.
+			WithEnvVariable("UNIQUE", uuid.New().String()).
+			// copy testdata import yaml from base
+			WithFile("import.yaml", seed).
+			WithServiceBinding("flipt", fliptToTest).
+			// it appears it takes a little while for Flipt to come online
+			// For the go tests they have to compile and that seems to be enough
+			// time for the target Flipt to come up.
+			// However, in this case the flipt binary is prebuilt and needs a little sleep.
+			WithExec([]string{"sh", "-c", fmt.Sprintf("sleep 2 && %s", strings.Join(importCmd, " "))}).
+			ExitCode(ctx)
+		if err != nil {
+			return err
+		}
+
+		// run readonly suite against imported Flipt instance
+		if err := suite(ctx, "readonly", base, fliptToTest, conf)(); err != nil {
+			return err
+		}
+
+		expected, err := seed.Contents(ctx)
+		if err != nil {
+			return err
+		}
+
+		namespace := conf.namespace
+		if namespace == "" {
+			namespace = "default"
+			// replace namespace in expected yaml
+			expected = strings.ReplaceAll(expected, "version: \"1.0\"\n", fmt.Sprintf("version: \"1.0\"\nnamespace: %s\n", namespace))
+		}
+
+		// use target flipt binary to invoke import
+		generated, err := flipt.
+			WithEnvVariable("UNIQUE", uuid.New().String()).
+			WithServiceBinding("flipt", fliptToTest).
+			WithExec(append([]string{"/flipt", "export", "-o", "/tmp/output.yaml"}, flags...)).
+			File("/tmp/output.yaml").
+			Contents(ctx)
+		if err != nil {
+			return err
+		}
+
+		// remove line that starts with comment character '#' and newline after
+		generated = generated[strings.Index(generated, "\n")+2:]
+
+		diff := cmp.Diff(expected, generated)
+		if diff != "" {
+			fmt.Println("Unexpected difference in exported output:")
+			fmt.Println(diff)
+			return errors.New("exported yaml did not match")
+		}
+
+		return nil
+	}
+}
+
+func suite(ctx context.Context, dir string, base, flipt *dagger.Container, conf testConfig) func() error {
+	return func() error {
+		flags := []string{"--flipt-addr", conf.address}
+		if conf.namespace != "" {
+			flags = append(flags, "--flipt-namespace", conf.namespace)
+		}
+
+		if conf.token != "" {
+			flags = append(flags, "--flipt-token", conf.token)
+		}
+
+		_, err := base.
+			WithServiceBinding("flipt", flipt).
+			WithWorkdir(path.Join("build/testing/integration", dir)).
+			WithEnvVariable("UNIQUE", uuid.New().String()).
+			WithExec(append([]string{"go", "test", "-v", "-race"}, append(flags, ".")...)).
+			ExitCode(ctx)
+
+		return err
+	}
+}
---
+++ ui/src/components/segments/ConstraintForm.tsx
@@ -0,0 +1,428 @@
+import { Dialog } from '@headlessui/react';
+import { QuestionMarkCircleIcon } from '@heroicons/react/20/solid';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+import { Form, Formik, useField, useFormikContext } from 'formik';
+import moment from 'moment';
+import { forwardRef, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import { selectCurrentNamespace } from '~/app/namespaces/namespacesSlice';
+import { selectTimezone } from '~/app/preferences/preferencesSlice';
+import Button from '~/components/forms/buttons/Button';
+import Input from '~/components/forms/Input';
+import Select from '~/components/forms/Select';
+import Loading from '~/components/Loading';
+import MoreInfo from '~/components/MoreInfo';
+import { createConstraint, updateConstraint } from '~/data/api';
+import { useError } from '~/data/hooks/error';
+import { useSuccess } from '~/data/hooks/success';
+import { requiredValidation } from '~/data/validations';
+import {
+  ComparisonType,
+  ConstraintBooleanOperators,
+  ConstraintDateTimeOperators,
+  ConstraintNumberOperators,
+  ConstraintStringOperators,
+  IConstraint,
+  IConstraintBase,
+  NoValueOperators
+} from '~/types/Constraint';
+import { Timezone } from '~/types/Preferences';
+
+const constraintComparisonTypes = () =>
+  (Object.keys(ComparisonType) as Array<keyof typeof ComparisonType>).map(
+    (t) => ({
+      value: t,
+      label: ComparisonType[t]
+    })
+  );
+
+const constraintOperators = (c: string) => {
+  let opts: Record<string, string> = {};
+  switch (ComparisonType[c as keyof typeof ComparisonType]) {
+    case ComparisonType.STRING_COMPARISON_TYPE:
+      opts = ConstraintStringOperators;
+      break;
+    case ComparisonType.NUMBER_COMPARISON_TYPE:
+      opts = ConstraintNumberOperators;
+      break;
+    case ComparisonType.BOOLEAN_COMPARISON_TYPE:
+      opts = ConstraintBooleanOperators;
+      break;
+    case ComparisonType.DATETIME_COMPARISON_TYPE:
+      opts = ConstraintDateTimeOperators;
+      break;
+  }
+  return Object.entries(opts).map(([k, v]) => ({
+    value: k,
+    label: v
+  }));
+};
+
+type ConstraintInputProps = {
+  name: string;
+  id: string;
+};
+
+type ConstraintOperatorSelectProps = ConstraintInputProps & {
+  onChange: (e: React.ChangeEvent<any>) => void;
+  type: string;
+};
+
+function ConstraintOperatorSelect(props: ConstraintOperatorSelectProps) {
+  const { onChange, type } = props;
+
+  const { setFieldValue } = useFormikContext();
+
+  const [field] = useField(props);
+
+  return (
+    <Select
+      className="mt-1"
+      {...field}
+      {...props}
+      onChange={(e) => {
+        setFieldValue(field.name, e.target.value);
+        onChange(e);
+      }}
+      options={constraintOperators(type)}
+    />
+  );
+}
+
+function ConstraintValueInput(props: ConstraintInputProps) {
+  const [field] = useField({
+    ...props,
+    validate: (value) => {
+      // value is required only if shown
+      return value ? undefined : 'Value is required';
+    }
+  });
+
+  return (
+    <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+      <div>
+        <label
+          htmlFor="value"
+          className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+        >
+          Value
+        </label>
+      </div>
+      <div className="sm:col-span-2">
+        <Input {...props} {...field} />
+      </div>
+    </div>
+  );
+}
+
+function ConstraintValueDateTimeInput(props: ConstraintInputProps) {
+  const { setFieldValue } = useFormikContext();
+  const timezone = useSelector(selectTimezone);
+
+  const [field] = useField({
+    ...props,
+    validate: (value) => {
+      const m = moment(value);
+      return m.isValid() ? undefined : 'Value is not a valid datetime';
+    }
+  });
+
+  const [fieldDate, setFieldDate] = useState(field.value?.split('T')[0] || '');
+  const [fieldTime, setFieldTime] = useState(field.value?.split('T')[1] || '');
+
+  useEffect(() => {
+    // if both date and time are set, then combine, parse, and set the value
+    if (
+      fieldDate &&
+      fieldDate.trim() !== '' &&
+      fieldTime &&
+      fieldTime.trim() !== ''
+    ) {
+      if (timezone === Timezone.LOCAL) {
+        // if local timezone, then parse as local (moment default) and convert to UTC
+        const m = moment(`${fieldDate}T${fieldTime}`);
+        setFieldValue(field.name, m.utc().format());
+        return;
+      }
+
+      // otherwise, parse as UTC
+      const m = moment.utc(`${fieldDate}T${fieldTime}`);
+      setFieldValue(field.name, m.format());
+      return;
+    }
+
+    // otherwise, if only date is set, then parse and set the value
+    if (fieldDate && fieldDate.trim() !== '') {
+      const m = moment(fieldDate);
+      setFieldValue(field.name, m.utc().format());
+    }
+  }, [timezone, field.name, fieldDate, fieldTime, setFieldValue]);
+
+  return (
+    <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+      <div>
+        <label
+          htmlFor="value"
+          className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+        >
+          Value
+        </label>
+        <span className="text-xs text-gray-400" id="value-tz">
+          <Link
+            to="/settings"
+            className="group inline-flex items-center text-gray-400 hover:text-gray-500"
+          >
+            <QuestionMarkCircleIcon
+              className="-ml-1 h-4 w-4 text-gray-300 group-hover:text-gray-400"
+              aria-hidden="true"
+            />
+            <span className="ml-1">{timezone}</span>
+          </Link>
+        </span>
+      </div>
+      <div className="sm:col-span-1">
+        <Input
+          type="date"
+          id="valueDate"
+          name="valueDate"
+          value={fieldDate}
+          onChange={(e) => {
+            setFieldDate(e.target.value);
+          }}
+        />
+      </div>
+      <div className="sm:col-span-1">
+        <Input
+          type="time"
+          id="valueTime"
+          name="valueTime"
+          value={
+            timezone === Timezone.LOCAL
+              ? moment(fieldTime, 'HH:mm Z').format('HH:mm')
+              : moment.utc(fieldTime, 'HH:mm Z').format('HH:mm')
+          }
+          onChange={(e) => {
+            setFieldTime(e.target.value);
+          }}
+        />
+      </div>
+      <input type="hidden" {...props} {...field} />
+    </div>
+  );
+}
+
+type ConstraintFormProps = {
+  setOpen: (open: boolean) => void;
+  segmentKey: string;
+  constraint?: IConstraint;
+  onSuccess: () => void;
+};
+
+const ConstraintForm = forwardRef((props: ConstraintFormProps, ref: any) => {
+  const { setOpen, segmentKey, constraint, onSuccess } = props;
+
+  const isNew = constraint === undefined;
+  const submitPhrase = isNew ? 'Create' : 'Update';
+  const title = isNew ? 'New Constraint' : 'Edit Constraint';
+
+  const { setError, clearError } = useError();
+  const { setSuccess } = useSuccess();
+
+  const [hasValue, setHasValue] = useState(true);
+  const [type, setType] = useState(
+    constraint?.type || 'STRING_COMPARISON_TYPE'
+  );
+
+  const namespace = useSelector(selectCurrentNamespace);
+
+  const initialValues = {
+    property: constraint?.property || '',
+    type: constraint?.type || ('STRING_COMPARISON_TYPE' as ComparisonType),
+    operator: constraint?.operator || 'eq',
+    value: constraint?.value || '',
+    description: constraint?.description || ''
+  };
+
+  const handleSubmit = async (values: IConstraintBase) => {
+    if (isNew) {
+      return createConstraint(namespace.key, segmentKey, values);
+    }
+    return updateConstraint(namespace.key, segmentKey, constraint?.id, values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      enableReinitialize={true}
+      onSubmit={(values, { setSubmitting }) => {
+        handleSubmit(values)
+          .then(() => {
+            clearError();
+            setSuccess(
+              `Successfully ${submitPhrase.toLocaleLowerCase()}d constraint`
+            );
+            onSuccess();
+          })
+          .catch((err) => {
+            setError(err);
+          })
+          .finally(() => {
+            setSubmitting(false);
+          });
+      }}
+      validationSchema={Yup.object({
+        property: requiredValidation
+      })}
+    >
+      {(formik) => (
+        <Form className="flex h-full flex-col overflow-y-scroll shadow-xl bg-white">
+          <div className="flex-1">
+            <div className="px-4 py-6 bg-gray-50 sm:px-6">
+              <div className="flex items-start justify-between space-x-3">
+                <div className="space-y-1">
+                  <Dialog.Title className="text-lg font-medium text-gray-900">
+                    {title}
+                  </Dialog.Title>
+                  <MoreInfo href="https://www.flipt.io/docs/concepts#constraints">
+                    Learn more about constraints
+                  </MoreInfo>
+                </div>
+                <div className="flex h-7 items-center">
+                  <button
+                    type="button"
+                    className="text-gray-400 hover:text-gray-500"
+                    onClick={() => {
+                      setOpen(false);
+                    }}
+                  >
+                    <span className="sr-only">Close panel</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+              <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                <div>
+                  <label
+                    htmlFor="property"
+                    className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+                  >
+                    Property
+                  </label>
+                </div>
+                <div className="sm:col-span-2">
+                  <Input name="property" id="property" forwardRef={ref} />
+                </div>
+              </div>
+              <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                <div>
+                  <label
+                    htmlFor="type"
+                    className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+                  >
+                    Type
+                  </label>
+                </div>
+                <div className="sm:col-span-2">
+                  <Select
+                    name="type"
+                    id="type"
+                    className="mt-1"
+                    value={formik.values.type}
+                    options={constraintComparisonTypes()}
+                    onChange={(e) => {
+                      const type = e.target.value as ComparisonType;
+                      formik.setFieldValue('type', type);
+                      setType(type);
+
+                      if (e.target.value === 'BOOLEAN_COMPARISON_TYPE') {
+                        formik.setFieldValue('operator', 'true');
+                        setHasValue(false);
+                      } else {
+                        formik.setFieldValue('operator', 'eq');
+                        setHasValue(true);
+                      }
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                <div>
+                  <label
+                    htmlFor="operator"
+                    className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+                  >
+                    Operator
+                  </label>
+                </div>
+                <div className="sm:col-span-2">
+                  <ConstraintOperatorSelect
+                    id="operator"
+                    name="operator"
+                    type={type}
+                    onChange={(e) => {
+                      const noValue = NoValueOperators.includes(e.target.value);
+                      setHasValue(!noValue);
+                    }}
+                  />
+                </div>
+              </div>
+              {hasValue && type != 'DATETIME_COMPARISON_TYPE' && (
+                <ConstraintValueInput name="value" id="value" />
+              )}
+              {hasValue && type === 'DATETIME_COMPARISON_TYPE' && (
+                <ConstraintValueDateTimeInput name="value" id="value" />
+              )}
+              <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                <div>
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+                  >
+                    Description
+                  </label>
+                  <span
+                    className="text-xs text-gray-400"
+                    id="description-optional"
+                  >
+                    Optional
+                  </span>
+                </div>
+                <div className="sm:col-span-2">
+                  <Input name="description" id="description" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex-shrink-0 border-t px-4 py-5 border-gray-200 sm:px-6">
+            <div className="flex justify-end space-x-3">
+              <Button
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                primary
+                type="submit"
+                className="min-w-[80px]"
+                disabled={
+                  !(formik.dirty && formik.isValid && !formik.isSubmitting)
+                }
+              >
+                {formik.isSubmitting ? <Loading isPrimary /> : submitPhrase}
+              </Button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+});
+
+ConstraintForm.displayName = 'ConstraintForm';
+export default ConstraintForm;
