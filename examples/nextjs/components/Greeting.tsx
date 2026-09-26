52bb1aa2b8b374274eebb985ad38726340ccc614
secaudit <secaudit@localhost>
2026-09-25 20:15:28 -0700

    secaudit secaudit/instance_flipt-io__flipt-5aef5a14890aa145c22d864a834694bae3a6f112/Claude_4.5_Sonnet__SWE-agent/before

---
+++ examples/nextjs/components/Greeting.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { useFlipt } from "../hooks/flipt";
+import { v4 as uuidv4 } from "uuid";
+
+export default function Greeting() {
+  const [data, setData] = useState<any | null>(null);
+
+  const flipt = useFlipt();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const evaluation = await flipt.evaluation.variant({
+          namespaceKey: "default",
+          flagKey: "language",
+          entityId: uuidv4(),
+          context: {},
+        });
+
+        let language = evaluation.variantKey;
+
+        const greeting =
+          language == "es"
+            ? "Hola, from Next.js client-side"
+            : "Bonjour, from Next.js client-side";
+
+        setData(greeting);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+  }, [flipt.evaluation]);
+
+  if (!data)
+    return <p className="text-xl font-bold align-middle"> Loading... </p>;
+
+  return <h1 className="text-3xl font-bold align-middle">{data}</h1>;
+}
