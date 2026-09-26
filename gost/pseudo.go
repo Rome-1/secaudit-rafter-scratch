966583a10538a42d097c26b279f5c5c8afc5db55
secaudit <secaudit@localhost>
2026-09-26 00:52:37 -0700

    secaudit secaudit/instance_future-architect__vuls-d576b6c6c15e56c47cc3e26f5878867677d4a9ea/Claude_4.5_Sonnet__SWE-agent/before

---
+++ gost/pseudo.go
@@ -0,0 +1,18 @@
+// +build !scanner
+
+package gost
+
+import (
+	"github.com/future-architect/vuls/models"
+	"github.com/knqyf263/gost/db"
+)
+
+// Pseudo is Gost client except for RedHat family and Debian
+type Pseudo struct {
+	Base
+}
+
+// DetectUnfixed fills cve information that has in Gost
+func (pse Pseudo) DetectUnfixed(driver db.DB, r *models.ScanResult, _ bool) (int, error) {
+	return 0, nil
+}
