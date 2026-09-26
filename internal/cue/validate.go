46eba21af6b3cd0c805edc61f8282ac787eaceb2
secaudit <secaudit@localhost>
2026-09-26 06:10:50 -0700

    secaudit secaudit/instance_flipt-io__flipt-b2170346dc37cf42fda1386cd630f24821ad2ac5/Claude_4_Sonnet__SWE-agent/gold

---
+++ internal/cue/validate.go
@@ -0,0 +1,118 @@
+package cue
+
+import (
+	_ "embed"
+	"errors"
+	"fmt"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+	cueerrors "cuelang.org/go/cue/errors"
+	"cuelang.org/go/encoding/yaml"
+)
+
+//go:embed flipt.cue
+var cueFile []byte
+
+// Location contains information about where an error has occurred during cue
+// validation.
+type Location struct {
+	File   string `json:"file,omitempty"`
+	Line   int    `json:"line"`
+	Column int    `json:"column"`
+}
+
+type unwrapable interface {
+	Unwrap() []error
+}
+
+// Unwrap checks for the version of Unwrap which returns a slice
+// see std errors package for details
+func Unwrap(err error) ([]error, bool) {
+	var u unwrapable
+	if !errors.As(err, &u) {
+		return nil, false
+	}
+
+	return u.Unwrap(), true
+}
+
+// Error is a collection of fields that represent positions in files where the user
+// has made some kind of error.
+type Error struct {
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
+}
+
+func (e Error) Format(f fmt.State, verb rune) {
+	if verb != 'v' {
+		f.Write([]byte(e.Error()))
+		return
+	}
+
+	fmt.Fprintf(f, `
+- Message  : %s
+  File     : %s
+  Line     : %d
+  Column   : %d
+`, e.Message, e.Location.File, e.Location.Line, e.Location.Column)
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("%s (%s %d:%d)", e.Message, e.Location.File, e.Location.Line, e.Location.Column)
+}
+
+type FeaturesValidator struct {
+	cue *cue.Context
+	v   cue.Value
+}
+
+func NewFeaturesValidator() (*FeaturesValidator, error) {
+	cctx := cuecontext.New()
+	v := cctx.CompileBytes(cueFile)
+	if v.Err() != nil {
+		return nil, v.Err()
+	}
+
+	return &FeaturesValidator{
+		cue: cctx,
+		v:   v,
+	}, nil
+}
+
+// Validate validates a YAML file against our cue definition of features.
+func (v FeaturesValidator) Validate(file string, b []byte) error {
+	f, err := yaml.Extract("", b)
+	if err != nil {
+		return err
+	}
+
+	yv := v.cue.BuildFile(f)
+	if err := yv.Err(); err != nil {
+		return err
+	}
+
+	err = v.v.
+		Unify(yv).
+		Validate(cue.All(), cue.Concrete(true))
+
+	var errs []error
+	for _, e := range cueerrors.Errors(err) {
+		rerr := Error{
+			Message: e.Error(),
+			Location: Location{
+				File: file,
+			},
+		}
+
+		if pos := cueerrors.Positions(e); len(pos) > 0 {
+			p := pos[len(pos)-1]
+			rerr.Location.Line = p.Line()
+			rerr.Location.Column = p.Column()
+		}
+
+		errs = append(errs, rerr)
+	}
+
+	return errors.Join(errs...)
+}
