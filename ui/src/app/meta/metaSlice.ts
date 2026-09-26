8c4ec816054bf5858e40644b8f5811c05896ebb8
secaudit <secaudit@localhost>
2026-09-26 06:07:51 -0700

    secaudit secaudit/instance_flipt-io__flipt-b2170346dc37cf42fda1386cd630f24821ad2ac5/Claude_4_Sonnet__SWE-agent/candidate

---
+++ ui/src/app/meta/metaSlice.ts
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getConfig, getInfo } from '~/data/api';
+import { IConfig, IInfo, StorageType } from '~/types/Meta';
+import { Theme } from '~/types/Preferences';
+
+interface IMetaSlice {
+  info: IInfo;
+  config: IConfig;
+}
+
+const initialState: IMetaSlice = {
+  info: {
+    version: '0.0.0',
+    latestVersion: '0.0.0',
+    latestVersionURL: '',
+    commit: '',
+    buildDate: '',
+    goVersion: '',
+    updateAvailable: false,
+    isRelease: false
+  },
+  config: {
+    storage: {
+      type: StorageType.DATABASE,
+      readOnly: false
+    },
+    ui: {
+      defaultTheme: Theme.SYSTEM
+    }
+  }
+};
+
+export const metaSlice = createSlice({
+  name: 'meta',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchInfoAsync.fulfilled, (state, action) => {
+        state.info = action.payload;
+      })
+      .addCase(fetchConfigAsync.fulfilled, (state, action) => {
+        state.config = action.payload;
+        if (action.payload.storage?.readOnly === undefined) {
+          state.config.storage.readOnly =
+            action.payload.storage?.type &&
+            action.payload.storage?.type !== StorageType.DATABASE;
+        }
+      });
+  }
+});
+
+export const selectInfo = (state: { meta: IMetaSlice }) => state.meta.info;
+export const selectConfig = (state: { meta: IMetaSlice }) => state.meta.config;
+export const selectReadonly = (state: { meta: IMetaSlice }) =>
+  state.meta.config.storage.readOnly;
+
+export const fetchInfoAsync = createAsyncThunk('meta/fetchInfo', async () => {
+  const response = await getInfo();
+  return response;
+});
+
+export const fetchConfigAsync = createAsyncThunk(
+  'meta/fetchConfig',
+  async () => {
+    const response = await getConfig();
+    return response;
+  }
+);
+
+export default metaSlice.reducer;
