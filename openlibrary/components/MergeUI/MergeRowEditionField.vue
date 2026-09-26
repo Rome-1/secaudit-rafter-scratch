cb981ab792982a802f542b25c21c98ed6b93b23c
secaudit <secaudit@localhost>
2026-09-26 04:17:38 -0700

    secaudit secaudit/instance_internetarchive__openlibrary-8a5a63af6e0be406aa6c8c9b6d5f28b2f1b6af5a-v0f5aece3601a5b4419f7ccec1dbda2071be28ee4/Claude_4.5_Sonnet__SWE-agent/before

---
+++ openlibrary/components/MergeUI/MergeRowEditionField.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="editions-wrapper">
+    <div
+      v-if="editions"
+      class="edition-count"
+    >
+      {{ editions[record.key].size }} edition{{ editions[record.key].size == 1 ? '' : 's' }}
+    </div>
+    <div
+      v-else-if="merged && record == merged.record"
+      class="edition-count"
+    >
+      {{ merged.edition_count }} edition{{ merged.edition_count == 1 ? '' : 's' }}
+    </div>
+    <div
+      v-if="editions"
+      class="td-container"
+    >
+      <EditionSnippet
+        v-for="edition in editions[record.key].entries"
+        :key="edition.key"
+        :edition="edition"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import EditionSnippet from './EditionSnippet.vue';
+
+export default {
+    components: {
+        EditionSnippet,
+    },
+    props: {
+        record: {
+            type: Object,
+            required: true
+        },
+        editions: {
+            type: Array,
+            required: true
+        },
+        merged: {
+            type: Object,
+            required: false
+        }
+    },
+};
+</script>
---
+++ openlibrary/olbase/__init__.py
@@ -0,0 +1,9 @@
+"""Infobase extension for Open Library."""
+
+from ..plugins import ol_infobase
+from . import events
+
+
+def init_plugin():
+    ol_infobase.init_plugin()
+    events.setup()
