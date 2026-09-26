9c7312b93c11bc5c4bc5fb833fc9e81c48eb1ee9
secaudit <secaudit@localhost>
2026-09-25 20:23:55 -0700

    secaudit secaudit/instance_flipt-io__flipt-5aef5a14890aa145c22d864a834694bae3a6f112/GPT-OSS__SWE-agent/candidate

---
+++ internal/ext/importer_fuzz_test.go
@@ -0,0 +1,28 @@
+//go:build go1.18
+// +build go1.18
+
+package ext
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"testing"
+)
+
+func FuzzImport(f *testing.F) {
+	testcases := []string{"testdata/import.yml", "testdata/import_no_attachment.yml", "testdata/export.yml"}
+
+	for _, tc := range testcases {
+		b, _ := os.ReadFile(tc)
+		f.Add(b)
+	}
+
+	f.Fuzz(func(t *testing.T, in []byte) {
+		importer := NewImporter(&mockCreator{})
+		if err := importer.Import(context.Background(), EncodingYAML, bytes.NewReader(in)); err != nil {
+			// we only care about panics
+			t.Skip()
+		}
+	})
+}
---
+++ ui/src/store.ts
@@ -0,0 +1,82 @@
+import {
+  configureStore,
+  createListenerMiddleware,
+  isAnyOf
+} from '@reduxjs/toolkit';
+
+import { flagsSlice } from './app/flags/flagsSlice';
+import { metaSlice } from './app/meta/metaSlice';
+import { namespacesSlice } from './app/namespaces/namespacesSlice';
+import {
+  preferencesKey,
+  preferencesSlice
+} from './app/preferences/preferencesSlice';
+import { segmentsApi } from './app/segments/segmentsApi';
+import { LoadingStatus } from './types/Meta';
+
+const listenerMiddleware = createListenerMiddleware();
+
+const namespaceKey = 'namespace';
+
+listenerMiddleware.startListening({
+  matcher: isAnyOf(
+    preferencesSlice.actions.themeChanged,
+    preferencesSlice.actions.timezoneChanged
+  ),
+  effect: (_action, api) => {
+    // save to local storage
+    localStorage.setItem(
+      preferencesKey,
+      JSON.stringify((api.getState() as RootState).preferences)
+    );
+  }
+});
+
+listenerMiddleware.startListening({
+  matcher: isAnyOf(namespacesSlice.actions.currentNamespaceChanged),
+  effect: (_action, api) => {
+    // save to local storage
+    localStorage.setItem(
+      namespaceKey,
+      (api.getState() as RootState).namespaces.currentNamespace
+    );
+  }
+});
+
+const preferencesState = JSON.parse(
+  localStorage.getItem(preferencesKey) || '{}'
+);
+
+const currentNamespace = localStorage.getItem(namespaceKey) || 'default';
+
+export const store = configureStore({
+  preloadedState: {
+    preferences: preferencesState,
+    namespaces: {
+      namespaces: {},
+      status: LoadingStatus.IDLE,
+      currentNamespace,
+      error: undefined
+    },
+    flags: {
+      status: LoadingStatus.IDLE,
+      flags: {},
+      error: undefined
+    }
+  },
+  reducer: {
+    namespaces: namespacesSlice.reducer,
+    preferences: preferencesSlice.reducer,
+    flags: flagsSlice.reducer,
+    meta: metaSlice.reducer,
+    [segmentsApi.reducerPath]: segmentsApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .prepend(listenerMiddleware.middleware)
+      .concat(segmentsApi.middleware)
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
